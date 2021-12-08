8b3156e2a482a423ae1ccc5817f6af592ec0e6db
PandaBurglar <zane.bokhari@gmail.com>
2021-12-07 23:59:01 -0500

    refactored code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const breedName = process.argv.slice(2);
 const { fetchBreedInformation } = require('./breedFetcher');
 
-// returns undefined not good...
 fetchBreedInformation(breedName, (err, desc) => {
   if (err) {
     console.log(err);
